1b3f9d0dcddef01e4048b1da6793c64eba3c42ee
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-01-23 11:32:33 -0300

    Patch: Changed some error codes, reviewd code structure
    Missing: Code architecture in middleware, routes, and controllers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
 import dotenv from "dotenv";
 import Joi from "joi";
@@ -31,7 +31,7 @@ app.get("/users", async (req, res) => {
         .find()
         .toArray()
         .then(data => { return res.send(data) })
-        .catch(() => { res.status(500).send("Erro!") });
+        .catch(() => { res.status(500).send("Erro no banco de dados") });
 });
 
 app.post("/users", async (req, res) => {
@@ -43,9 +43,16 @@ app.post("/users", async (req, res) => {
         email: Joi.string().required(),
         password: Joi.string().required(),
         confirmation: Joi.string().valid(Joi.ref("password")).required()
-    })
+    });
 
-    const userValidation = userSChema.validate({ name, email, password, confirmation }, { abortEarly: false })
+    const userValidation = userSChema.validate(
+        {
+            name,
+            email,
+            password,
+            confirmation
+        },
+        { abortEarly: false });
 
     if (userValidation.error) {
         res.status(422).send(userValidation.error.details);
@@ -71,9 +78,9 @@ app.post("/users", async (req, res) => {
         try {
             const user = { name, email, password: passwordHash };
             await db.collection("users").insertOne(user);
-            res.status(201).send("Usuário criado com sucesso!")
+            res.status(201).send("Usuário criado com sucesso!");
         } catch {
-            console.log("Error adding users")
+            res.status(500).send("Erro no banco de dados");
         }
     }
 })
@@ -86,7 +93,12 @@ app.post("/sessions", async (req, res) => {
         password: Joi.string().required(),
     })
 
-    const userValidation = userSChema.validate({ email, password }, { abortEarly: false })
+    const userValidation = userSChema.validate(
+        {
+            email,
+            password
+        },
+        { abortEarly: false });
 
     if (userValidation.error) {
         res.status(422).send(userValidation.error.details);
@@ -97,20 +109,18 @@ app.post("/sessions", async (req, res) => {
     password = stripHtml(password).result.trim();
 
     const user = await db.collection("users").findOne({ email });
-    const name = user.name
+    const name = user.name;
 
     if (user && bcrypt.compareSync(password, user.password)) {
         const token = uuid();
         delete user.password;
         await db.collection("sessions").insertOne({
             userId: user._id, token
-        })
-
-        const body = { token, name }
-
-        res.status(201).send(body)
+        });
+        const body = { token, name };
+        res.status(201).send(body);
     } else {
-        res.status(404).send("Usuário não encontrado. Email ou senha incorretos")
+        res.status(404).send("Usuário não encontrado. Email ou senha incorretos");
     }
 })
 
@@ -118,19 +128,19 @@ app.get("/transactions", async (req, res) => {
     const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
 
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.status(401).send("Credenciais de autentição inválidas");
 
     const session = await db.collection("sessions").findOne({ token });
 
     if (!session) {
-        return res.sendStatus(401);
+        return res.status(401).send("Credenciais de autentição inválidas");
     }
 
     const user = await db.collection("users").findOne({
         _id: session.userId
-    })
+    });
 
-    const id = session.userId
+    const id = session.userId;
 
     if (user) {
         await db.collection("transactions")
@@ -139,32 +149,29 @@ app.get("/transactions", async (req, res) => {
             })
             .toArray()
             .then(data => { return res.status(200).send(data) })
-            .catch(() => { res.status(500).send("Erro!") });
+            .catch(() => { res.status(500).send("Erro ao executar requisição") });
     } else {
-        res.status(401).send("Erro!");
+        res.status(401).send("Credenciais de autentição inválidas");
     }
-
-
 })
 
 app.post("/transactions", async (req, res) => {
-    const { amount, description, type } = req.body
+    const { amount, description, type } = req.body;
     const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
 
     const transactionSchema = Joi.object({
-        amount: Joi.number().precision(2).options({ convert: false }).required(),
+        amount: Joi.string().replace(',', '.', true).regex(/^\d+(\.\d{1,2})?$/).required(),
         description: Joi.string().required(),
         type: Joi.string().valid("entry", "exit").required()
-    })
+    });
 
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.status(401).send("Credenciais de autentição inválidas");
 
     const session = await db.collection("sessions").findOne({ token });
 
-
     if (!session) {
-        return res.sendStatus(401);
+        return res.status(401).send("Credenciais de autentição inválidas");
     }
 
     const user = await db.collection("users").findOne({
@@ -174,7 +181,7 @@ app.post("/transactions", async (req, res) => {
 
     const transactionValidation = transactionSchema.validate(
         { amount, description, type },
-        { abortEarly: false })
+        { abortEarly: false });
 
     if (transactionValidation.error) {
         res.status(422).send(transactionValidation.error.details);
@@ -182,8 +189,8 @@ app.post("/transactions", async (req, res) => {
     }
 
     if (user) {
-        let value = Number(amount)
-        value = value.toFixed(2)
+        let value = Number(amount);
+        value = value.toFixed(2);
         delete user.password;
         await db.collection("transactions").insertOne({
             userId: session.userId,
@@ -192,7 +199,7 @@ app.post("/transactions", async (req, res) => {
             type,
             date
         })
-        res.status(201).send("Transação efetuada com sucesso!")
+        res.status(201).send("Transação efetuada com sucesso!");
     } else {
         res.sendStatus(401);
     }
@@ -226,5 +233,5 @@ app.get("/sessions", async (req, res) => {
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+    console.log(`Server running on port ${process.env.PORT}`);
+});
